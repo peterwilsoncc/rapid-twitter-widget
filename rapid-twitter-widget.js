f55f2aea7d71d925d2238268dfa934ecaf0cdca4
Peter Wilson <git-commit-by@peterwilson.cc>
2013-01-20 01:52:13 +1100

    make reply class local

--- rapid-twitter-widget.js
+++ rapid-twitter-widget.js
@@ -133,7 +133,7 @@ RapidTwitter.script = function(RapidTwitter, window, document) {
 								return (elem.display_url)? '<a href="' + elem.url + '" class="tweet__link" title="' + elem.expanded_url + '">' + elem.display_url + '</a>': elem.url;
 								break;
 							case 'user_mentions':
-								reply_class = (elem.indices[0] == 0) ? ' tweet__mention--reply' : '';
+								var reply_class = (elem.indices[0] == 0) ? ' tweet__mention--reply' : '';
 								return '<a href="https://twitter.com/' + elem.screen_name + '" class="tweet__mention'+reply_class+'"><span>@</span>' + elem.screen_name + '</a>';
 								break;
 							case 'hashtags':
