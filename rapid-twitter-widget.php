17225dd108913d63d2b6cff00a291240a2fe4ce2
Peter Wilson <git-commit-by@peterwilson.cc>
2015-08-08 17:32:13 +1000

    Revert @Djelibeybi commits - they are in the dev branch now

--- rapid-twitter-widget.php
+++ rapid-twitter-widget.php
@@ -33,13 +33,6 @@ class Rapid_Twitter_Widget extends WP_Widget {
 			return;
 		}
 		echo "<style>.widget_twitter--hidden{display:none!important;}</style>";
-		wp_register_style(
-			'rapid-twitter-widget',
-			plugins_url( 'rapid-twitter-widget/rapid-twitter-widget' . '.css' ),
-			'',
-			RAPID_TWITTER_WIDGET_VERSION,
-			'all'
-		);
 	}
 
 	function rapid_twitter_widget_script() {
@@ -78,7 +71,6 @@ class Rapid_Twitter_Widget extends WP_Widget {
 		$instance['show'] = absint( $new_instance['show'] );
 		$instance['hidereplies'] = isset( $new_instance['hidereplies'] );
 		$instance['includeretweets'] = isset( $new_instance['includeretweets'] );
-		$instance['followbutton'] = isset( $new_instance['followbutton'] );
 
 		return $instance;
 	}
@@ -90,8 +82,7 @@ class Rapid_Twitter_Widget extends WP_Widget {
 				'account'     => '',
 				'title'       => '',
 				'show'        => 5,
-				'hidereplies'	=> false,
-				'followbutton'  => false,
+				'hidereplies'	=> false
 			) );
 
 		$account = esc_attr( $instance['account'] );
@@ -101,7 +92,6 @@ class Rapid_Twitter_Widget extends WP_Widget {
 			$show = 5;
 		$hidereplies = (bool) $instance['hidereplies'];
 		$include_retweets = (bool) $instance['includeretweets'];
-		$follow_button = (bool) $instance['followbutton'];
 
 		//Title
 		echo '<p>';
@@ -126,7 +116,7 @@ class Rapid_Twitter_Widget extends WP_Widget {
 		echo '</select>';
 		echo '</p>';
 
-		//Hide Replies
+		//Hide Reploes
 		echo '<p>';
 		echo '<label for="' . $this->get_field_id('hidereplies') . '">';
 		echo '<input id="' . $this->get_field_id('hidereplies') . '" class="checkbox" type="checkbox" name="' . $this->get_field_name('hidereplies') . '"';
@@ -142,15 +132,6 @@ class Rapid_Twitter_Widget extends WP_Widget {
 		if ( $include_retweets )
 			echo ' checked="checked"';
 		echo ' /> ' . esc_html__('Include retweets');
-		echo '</label>';
-		echo '</p>';
-
-		//Follow button
-		echo '<p>';
-		echo '<label for="' . $this->get_field_id('followbutton') . '"><input id="' . $this->get_field_id('followbutton') . '" class="checkbox" type="checkbox" name="' . $this->get_field_name('followbutton') . '"';
-		if ( $follow_button )
-			echo ' checked="checked"';
-		echo ' /> ' . esc_html__('Display Follow button');
 		echo '</label>';
 		echo '</p>';
 	}
@@ -170,16 +151,11 @@ class Rapid_Twitter_Widget extends WP_Widget {
 		}
 		$hidereplies = (bool) $instance['hidereplies'] ? 't' : 'f';
 		$include_retweets = (bool) $instance['includeretweets'] ? 't' : 'f';
-		$follow_button = (bool) $instance['followbutton'];
 
 		echo $before_widget;
 
 		echo $before_title;
-		if ( $follow_button ) {
-			echo esc_html($title);
-		} else {
-			echo "<a href='" . esc_url( "https://twitter.com/{$account}" ) . "'>" . esc_html($title) . "</a>";
-		}
+		echo "<a href='" . esc_url( "https://twitter.com/{$account}" ) . "'>" . esc_html($title) . "</a>";
 		echo $after_title;
 
 
@@ -211,7 +187,7 @@ class Rapid_Twitter_Widget extends WP_Widget {
 		$script_id = hash( 'md5', $widget_ref );
 		$script_id = base_convert( $script_id, 16, 36 );
 
-		echo '<script>';
+		echo '<script id="' . $script_id . '">';
 		echo 'if(typeof(RapidTwitter)==\'undefined\'){';
 		echo 'RapidTwitter={};RapidTwitter.apis={};';
 		echo '}';
@@ -226,13 +202,7 @@ class Rapid_Twitter_Widget extends WP_Widget {
 		echo '}';
 		echo 'RapidTwitter.apis.' . $url_ref . '.widgets.push(\'' . $script_id . '\');';
 		echo '</script>';
-		echo '<div id="' . $script_id . '"></div>';
 		wp_enqueue_script( 'rapid-twitter-widget' );
-		if ( $follow_button ) {
-			wp_enqueue_style( 'rapid-twitter-widget' );
-			echo '<a target="_blank" class="rapid-twitter-btn" title="Follow @' . $account . ' on Twitter" href="https://twitter.com/' . $account . '">' . "\n";
-			echo '<i></i><span class="label">Follow @' . $account . '</span></a>' . "\n";
-		}
 		echo $after_widget;
 		
 	}
@@ -335,19 +305,11 @@ class Rapid_Twitter_Controller {
 	}
 
 	function output_key_field() {
-		if ( ! defined('RAPID_TWITTER_KEY') ) {
-			$this->output_text_field( 'key' );
-		} else {
-			echo RAPID_TWITTER_KEY . '<br /><em>Globally defined in wp-config.php.</em>';
-		}
+		$this->output_text_field( 'key' );
 	}
 	
 	function output_secret_field(){
-		if ( ! defined('RAPID_TWITTER_SECRET') ) {
-			$this->output_text_field( 'secret' );
-		} else {
-			echo RAPID_TWITTER_SECRET . '<br /><em>Globally defined in wp-config.php.</em>';
-		}
+		$this->output_text_field( 'secret' );
 	}
 	
 	function output_text_field( $id ) {
@@ -362,12 +324,7 @@ class Rapid_Twitter_Controller {
 	function get_options() {
 		$options = &$this->options;
 		
-		if ( ! defined( 'RAPID_TWITTER_KEY' ) && ! defined ( 'RAPID_TWITTER_SECRET' ) ) {
-			$options = get_option( 'rapid_twitter_widget_api' );
-		} else {
-			$options['key'] = RAPID_TWITTER_KEY;
-			$options['secret'] = RAPID_TWITTER_SECRET;
-		}
+		$options = get_option( 'rapid_twitter_widget_api' );
 		
 		if ( $options['key'] AND $options['secret'] ) {
 			// get the token
@@ -395,7 +352,7 @@ class Rapid_Twitter_Controller {
 		
 		$signature = base64_encode( $key_encode . ':' . $secret_encode );
 		
-		$token = get_site_transient( 'rapid_twitter_widget_token' );
+		$token = get_transient( 'rapid_twitter_widget_token' );
 		
 		if ( $token AND ( $token['signature'] != $signature ) ) {
 			unset( $token );
@@ -422,7 +379,7 @@ class Rapid_Twitter_Controller {
 				$token = json_decode( $response_body, true );
 				$token['signature'] = $signature;
 				
-				set_site_transient('rapid_twitter_widget_token', $token, 60*60*12 );
+				set_transient('rapid_twitter_widget_token', $token, 60*60*12 );
 				
 			}
 		}
@@ -538,7 +495,7 @@ class Rapid_Twitter_Controller {
 		$transient_backup = substr( $transient_backup, 0, 45 );
 		
 		
-		$tweets = get_site_transient( $transient_name );
+		$tweets = get_transient( $transient_name );
 		
 		if ( !$tweets ) {
 			$response = wp_remote_get( $http_url, $http_args );
@@ -555,7 +512,7 @@ class Rapid_Twitter_Controller {
 						$tweet_cache_expire = 300;
 						break;
 					} else {
-						set_site_transient( $transient_backup, $tweets, 86400 ); // A one day backup in case there is trouble talking to Twitter
+						set_transient( $transient_backup, $tweets, 86400 ); // A one day backup in case there is trouble talking to Twitter
 					}
 
 					$cache_for =  900; 
@@ -581,13 +538,13 @@ class Rapid_Twitter_Controller {
 					break;
 				
 				default :  // display an error message
-					$tweets = get_site_transient( $transient_backup );
+					$tweets = get_transient( $transient_backup );
 					$cache_for = 300;
 					break;
 			}
 			
 			if ( $cache_for != 0 ) {
-				set_site_transient( $transient_name, $tweets, $cache_for ); /* cache for 5 min */
+				set_transient( $transient_name, $tweets, $cache_for ); /* cache for 5 min */
 			}
 			
 		}
